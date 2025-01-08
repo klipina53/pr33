991f1deac126ca059a013024bec4966582cb325c
klipina53 <klipina53@gmail.com>
2025-01-08 18:39:05 +0500

    Разработанный пользовательский элемент Message. Lipina

---
+++ pr33-ChatStudents_Lipina/Pages/Items/Message.xaml.cs
@@ -0,0 +1,20 @@
+﻿using pr33_ChatStudents_Lipina.Classes.Common;
+using pr33_ChatStudents_Lipina.Models;
+using System.Windows.Controls;
+
+namespace pr33_ChatStudents_Lipina.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages Messages, Users UserFrom)
+        {
+            InitializeComponent();
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            FIO.Content = UserFrom.ToFIO();
+            tbMessage.Text = Messages.Message;
+        }
+    }
+}
