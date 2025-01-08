aafc168684ba536c1603cda68d63e83bf0b5a345
klipina53 <klipina53@gmail.com>
2025-01-08 17:16:41 +0500

    Разработанные модели.Липина

---
+++ pr33-ChatStudents_Lipina/Models/Messages.cs
@@ -0,0 +1,16 @@
+﻿namespace pr33_ChatStudents_Lipina.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int UserFrom { get; set; }
+        public int UserTo { get; set; }
+        public string Message { get; set; }
+        public Messages(int UserFrom, int UserTo, string Message)
+        {
+            this.UserFrom = UserFrom;
+            this.UserTo = UserTo;
+            this.Message = Message;
+        }
+    }
+}
---
+++ pr33-ChatStudents_Lipina/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿namespace pr33_ChatStudents_Lipina.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public byte[] Photo { get; set; }
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
