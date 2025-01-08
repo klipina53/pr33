daaf9137bc849fff75abd1e3436da3e543f22193
klipina53 <klipina53@gmail.com>
2025-01-08 17:00:20 +0500

    Добавьте файлы проекта.

---
+++ pr33-ChatStudents_Lipina/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace pr33_ChatStudents_Lipina
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
