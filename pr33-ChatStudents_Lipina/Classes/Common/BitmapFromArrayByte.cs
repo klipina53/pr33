a8ac6cb5cb1c7f3fad2ab577bd33498444137c84
klipina53 <klipina53@gmail.com>
2025-01-08 17:22:36 +0500

    Разработанные общие классы. Липина

---
+++ pr33-ChatStudents_Lipina/Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace pr33_ChatStudents_Lipina.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using (var Stream = new MemoryStream(imageData))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource = null;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }   
+    }
+}
---
+++ pr33-ChatStudents_Lipina/Classes/Common/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr33_ChatStudents_Lipina.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=student.permaviat.ru;" +
+          "Trusted_Connection=False;" +
+          "Database=base2_ISP_21_2_17;" +
+          "User=ISP_21_2_17;" +
+          "Pwd=Tz4ZP5o6W#;";
+    }
+}
