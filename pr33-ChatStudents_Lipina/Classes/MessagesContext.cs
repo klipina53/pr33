fed6516e6ae4a26f805ad474867510a5f9322ef3
klipina53 <klipina53@gmail.com>
2025-01-08 17:26:46 +0500

    Разработанные контексты данных.Lipina

---
+++ pr33-ChatStudents_Lipina/Classes/MessagesContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pr33_ChatStudents_Lipina.Classes.Common;
+using pr33_ChatStudents_Lipina.Models;
+
+namespace pr33_ChatStudents_Lipina.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public MessagesContext() =>
+            Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ pr33-ChatStudents_Lipina/Classes/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pr33_ChatStudents_Lipina.Classes.Common;
+using pr33_ChatStudents_Lipina.Models;
+
+namespace pr33_ChatStudents_Lipina.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext() =>
+            Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+
+
+    }
+}
